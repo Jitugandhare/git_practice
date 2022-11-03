67280114771cc19c16ee39484eaaa5f44faa7c6a
Rita0156 <ritachaudhari006@gmail.com>
2022-11-03 08:58:50 +0530

    update_file

--- prime.js
+++ prime.js
@@ -1,9 +1,19 @@
+let count=0;
 function checkprime(num){
-	for(let i=0;i<=num;i++){
-		if(num%i==){
+
+	for(let i=1;i<=num;i++){
+		if(num%i==0){
 			return true;
+          count++
 		}
-	}
-	return false;
+	else {
+   return false;
+          }
+	
 }
 checkprime(23);
+if(count==2){
+console.log("prime number")
+} else {
+console.log("not prime")
+}
